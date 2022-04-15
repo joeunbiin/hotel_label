b8f6b9a5cdc7613733213d13ed3bed798131e8d4
joeunbiin <102141487+joeunbiin@users.noreply.github.com>
2022-04-15 19:52:36 +0900

    ver.1.3

--- js/index.js
+++ js/index.js
@@ -35,34 +35,30 @@ $('#article1 .playstop').on('click', function () {
 })
 
 
-var article2Near = $('#article2').offset().top - (wh / 2+100)
-var article3Near = $('#article3').offset().top - (wh / 2+100)
-// css에서 fixed 하지말고 on붙으면 이렇게 스크롤바내려오면 되도록 설정
-$(window).on('scroll', function () {
-    var sct = $(this).scrollTop()
-    // if (sct >= 50 && !$('#header').hasClass('on')) {
-    //     $('#header').addClass('on')
-    //     $('.gotop').addClass('on')
-    // } else if (sct < 50 && $('#header').hasClass('on')) {
-    //     $('#header').removeClass('on')
-    //     $('.gotop').removeClass('on')
-    // }
 
-    // article2
-    if (sct >= article2Near && !$('#article2').hasClass('on')) {
-        $('#article2').addClass('on')
-    } else if (sct === 0) {
-        $('#article2').removeClass('on')
+// rooms li
+var num;
+$('.roomlist > li').on('click', function () {
+    $(this).addClass('active')
+        .siblings().removeClass('active')
+    num = $(this).index()
+    $(this).parent().next().children()
+        .eq(num).addClass('active')
+        .siblings().removeClass('active')
+})
+
+// 슬라이드
+$(".slide_group2").slick({
+    autoplay: false, // 자동재생
+    autoplaySpeed: 4000, // 간격시간
+    dots: true, // 동그라미버튼
+    pauseOnFocus: false, // 동그라미번호버튼 클릭시 자동실행 멈춤여부
+    prevArrow: '<button class="prev"><i class="fas fa-angle-left"></i></button>',
+    nextArrow: '<button class="next"><i class="fas fa-angle-right"></i></button>',
+    responsive: [{
+        breakpoint: 1025,
+        settings: {arrows: false},
+        
     }
-
-
-    // article3
-    if (sct >= article3Near && !$('#article3').hasClass('on')) {
-        $('#article3').addClass('on')
-    } else if (sct === 0) {
-        $('#article3').removeClass('on')
-    }
-
-
-    
+]
 })
